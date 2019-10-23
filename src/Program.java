d8fbac19301a2bbc8b8dfae18c9c23e7dc68eb34
andrian <andrianwahyu41@gmail.com>
2019-10-23 18:09:32 +0700

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,40 @@
+
+
+public class Program {
+    public static void main(String[] args) {
+        ClassQueue antri = new ClassQueue(5);
+
+        antri.masuk("Andrian");
+        antri.masuk("Wira");
+        antri.masuk("Rozi");
+        System.out.println("=====================================");
+
+        System.out.println(antri.keluar()+" Selesai");
+
+        System.out.println();
+        System.out.println();
+        antri.print();
+        System.out.println("========================");
+
+        antri.masuk("ridho");
+
+        System.out.println("============================");
+        antri.print();
+        System.out.println("============================");
+
+        antri.masuk("Badri");
+        System.out.println("====================================   ");
+        antri.print();
+        System.out.println("========================================");
+
+        System.out.println(antri.keluar() + " Selesai");
+
+        System.out.println();
+        antri.masuk("Yudha");
+        System.out.println();
+        antri.print();
+
+
+
+    }
+}
